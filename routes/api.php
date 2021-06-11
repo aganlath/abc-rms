9f460c7e12831fd7c2cb7318eb27ade46b1c17d1
a.ganlath <a.ganlath@temper.works>
2021-06-11 14:43:33 +0530

    User controller update

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,8 @@ Route::name('api.')
 
         Route::middleware('auth:sanctum')->group(function () {
             Route::apiResource('customers', 'CustomerController')->except('show');
-            Route::apiResource('users', 'UserController')->except('show');
+            Route::apiResource('users', 'UserController')->only('index');
+            Route::post('users/upload_csv', 'UserController@upload_csv')->name('users.upload_csv');
         });
     });
 
